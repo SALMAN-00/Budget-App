356988833b64c3a54ce49aee546ac07a527cc102
iplmr <45397969+iplmr@users.noreply.github.com>
2024-03-05 12:09:19 +0300

    create new component called Expense inside it i wite form and save value, using map to print value in the page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import IncomForm from './components/IncomForm';
+import Expense from './components/Expense';
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
   return (
 <>
 
-<IncomForm/>
+<Expense/>
 
 </>
   );
---
+++ src/components/Expense.tsx
@@ -0,0 +1,88 @@
+import { log } from "console";
+import React, { useState } from "react";
+export default function Expense() {
+  type Expense = {
+    source: string;
+    amount: number;
+    date: string;
+  };
+  const [expense, setExpense] = useState({
+    source: "",
+    amount: 0,
+    date: "",
+  });
+
+  const [expenses, setExpenses] = useState<Expense[]>([]);
+
+  const handelSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newExpense = {
+      source: expense.source,
+      amount: expense.amount,
+      date: expense.date,
+    };
+    setExpenses([...expenses, newExpense]);
+    setExpense({
+      source: "",
+      amount: 0,
+      date: "",
+    });
+  };
+  return (
+    <>
+      <div>
+        <form onSubmit={handelSubmit}>
+          <div>
+            <label htmlFor="expense-source">Expense source</label>
+            <input
+              type="text"
+              id="expense-source"
+              placeholder="Phone bill"
+              value={expense.source}
+              onChange={e =>
+                setExpense(prev => ({ ...prev, source: e.target.value }))
+              }
+            />
+
+            <label htmlFor="amount-of-expense">Amount of expense</label>
+            <input
+              type="text"
+              id="amount-of-expense"
+              value={expense.amount}
+              onChange={e =>
+                setExpense(prev => ({
+                  ...prev,
+                  amount: parseInt(e.target.value),
+                }))
+              }
+            />
+
+            <label htmlFor="date-of-expense">Date of expense</label>
+            <input
+              type="date"
+              id="date-of-expense"
+              value={expense.date}
+              onChange={e =>
+                setExpense(prev => ({ ...prev, date: e.target.value }))
+              }
+            />
+
+            <button type="submit">Add expense</button>
+            {expenses.map(item=>{
+                console.log('item expense running');
+                return(
+                    <ul>
+
+                        <li>{item.source}</li>
+                        <li>{item.amount}</li>
+                        <li>{item.date}</li>
+                    </ul>
+                )
+            })}
+          </div>
+        </form>
+
+      </div>
+    </>
+  );
+}
