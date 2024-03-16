cb9e2514a96036f47606c4805d86f01757e600b4
iplmr <45397969+iplmr@users.noreply.github.com>
2024-03-16 09:29:39 +0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+
+import IncomeWrapper from "./components/IncomeWrapper";
+import ExpenseWrapper from "./components/ExpenseWrapper";
 
 export default function App() {
   return (
     <>
-    <div></div>
+      <div className="flex">
+        <IncomeWrapper />
+        <ExpenseWrapper />
+      </div>
     </>
-  )
+  );
 }
