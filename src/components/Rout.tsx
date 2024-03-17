05dc1ddf09f580b96e87393bad95630659f4b1c1
iplmr <45397969+iplmr@users.noreply.github.com>
2024-03-17 05:27:23 +0300

    adding ErorrPage.tsx

--- src/components/Rout.tsx
+++ src/components/Rout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import ErorrPage from "../pages/ErorrPage";
 import Home from "../pages/Home";
 
 export default function Rout() {
@@ -8,7 +9,7 @@ export default function Rout() {
     <>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        
+        <Route path="/*" element={<ErorrPage/>}/>
       </Routes>
     </>
   );
---
+++ src/pages/ErorrPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function ErorrPage() {
+  return (
+    <>
+      <div className="flex  justify-center">
+        <h1 className="text-3xl">Erorr 404 this page not found</h1>
+      </div>
+    </>
+  );
+}
